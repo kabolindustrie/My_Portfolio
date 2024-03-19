ba172b17024543c9d9852ee7d19d164ea068f495
Kabol Amine <amine.kabol@gmail.com>
2024-03-19 14:49:41 +0100

    mise en place

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+        'lora': ['Lora', 'sans-serif'],
+      },
+      colors: {
+        main: "#292736",
+        "main-light": "#4A4856",
+        "red-dark": "#D41313",
+        "red-light": "#F4C4C4",
+        "gray-light": "#E6E6E6",
+        "gray-dark": "#626262",
+      }
+    },
+  },
+  plugins: [],
+}
