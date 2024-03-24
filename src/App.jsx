b57c19659f71f4c3a2e9c3010cc75d7741c7ae9d
Kabol Amine <amine.kabol@gmail.com>
2024-03-24 12:41:14 +0100

    section trust down à voir si on la garde

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Skill from './Components/Skill'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar />
     <Hero />
+    <Skill />
     </>
   )
 }
---
+++ src/Components/Skill.jsx
@@ -0,0 +1,33 @@
+
+import Google from '../assets/images/company/Google.png';
+import Spotify from '../assets/images/company/Spotify.png';
+import Stripe from '../assets/images/company/Stripe.png';
+import Lyft from '../assets/images/company/Lyft.png';
+
+export default function Skill() {
+  return (
+    <div className="bg-main pt-14 pb-12 px-4 md:pt-16 lb:pb-20">
+      <p className="italic font-lora text-center text-gray-light text-sm mb-4 md:text-lg">
+        Des particuliers aux entreprises
+      </p>
+      <h2 className="text-white font-lora relative z-0 text-2xl w-fit mx-auto mb-7 sm:text-4xl sm:mb-10 md:text-5xl md:mb-12
+      after:absolute after:w-full after:h-2 after:bottom-0 after:left-0 after:-z-10 after:bg-main-light sm:after:h-5 sm:after:-bottom-2
+      ">
+        Ils ont fait confiance </h2>
+        <div className="grid grid-cols-2 mt-8 max-w-sm mx-auto sm:grid-cols-4 sm:max-w-xl lg:max-w-2xl">
+          <div className="flex justify-center">
+            <img className="h-16 sm:h-20" src={Google} alt="Logo Google" />
+          </div>
+          <div className="flex justify-center">
+            <img className="h-16 sm:h-20" src={Spotify} alt="Logo Google" />
+          </div>
+          <div className="flex justify-center">
+            <img className="h-16 sm:h-20" src={Stripe} alt="Logo Google" />
+          </div>
+          <div className="flex justify-center">
+            <img className="h-16 sm:h-20" src={Lyft} alt="Logo Google" />
+          </div>
+        </div>
+    </div>
+  )
+}
